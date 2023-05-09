10f049801db2b26efaa69150602d07f1f4f637cc
Kristen Jacobsen <kristen.jacobsen@cqumail.com>
2023-05-09 20:49:05 +1000

    Attempted to get the WebServer to access the database, updating the files that I had

---
+++ IBDMSWebServer/src/main/java/com/mycompany/assignment1/service/ApplicationConfig.java
@@ -0,0 +1,36 @@
+package com.mycompany.assignment1.service;
+
+import java.util.Set;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Kristen
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+@Path("com.mycompany.assignment1.drone")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.mycompany.assignment1.service.ApplicationConfig.class);
+        resources.add(com.mycompany.assignment1.service.DroneFacadeREST.class);
+        resources.add(com.mycompany.assignment1.service.FireFacadeREST.class);
+        resources.add(com.mycompany.assignment1.service.FiretrucksFacadeREST.class);
+        resources.add(org.eclipse.persistence.jaxb.rs.MOXyJsonProvider.class);
+    }
+    
+}
